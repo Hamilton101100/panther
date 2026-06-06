2098a9a0ad78467a7ce939d4f779ef159972adba
Hamilton <josejaminton599@gmail.com>
2026-06-06 14:06:38 -0500

    actualizar consultas SQL en Profesion para incluir el campo 'area' en las operaciones de inserción y actualización

--- rest/querys/business/Profesion.php
+++ rest/querys/business/Profesion.php
@@ -1,4 +1,4 @@
 <?php
-define("INSERT_PROFESION", "INSERT INTO Profesion (nombre) VALUES (?)");
-define("UPDATE_PROFESION", "UPDATE Profesion SET nombre=? WHERE id=?");
+define("INSERT_PROFESION", "INSERT INTO Profesion (nombre, area) VALUES (?,?)");
+define("UPDATE_PROFESION", "UPDATE Profesion SET nombre=?, area=? WHERE id=?");
 ?>
