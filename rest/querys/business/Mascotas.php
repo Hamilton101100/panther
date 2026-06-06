8c75e3fccc8dd24336f9a21a7283fac84aa4d51c
Hamilton <josejaminton599@gmail.com>
2026-06-06 12:42:30 -0500

    agregar definiciones de consultas para insertar y actualizar mascotas y profesiones

---
+++ rest/querys/business/Mascotas.php
@@ -0,0 +1,3 @@
+<?php
+define("INSERT_MASCOTAS", "INSERT INTO mascotas (nombre, raza) VALUES (?,?)");
+define("UPDATE_MASCOTAS", "UPDATE mascotas SET nombre=?, raza=? WHERE id=?;");
---
+++ rest/querys/business/Profesion.php
@@ -0,0 +1,3 @@
+<?php
+define("INSERT_PROFESION", "INSERT INTO profesiones (nombre) VALUES (?)");
+define("UPDATE_PROFESION", "UPDATE profesiones SET nombre=? WHERE id=?;");
